8dbbbf01077ea237ab10d5164ab92924047290d8
dayoadeyemi <dayoadeyemi1991@hotmail.com>
2017-07-13 13:13:37 +0100

    updated

--- profiles/management/commands/import_data.py
+++ profiles/management/commands/import_data.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 import csv, os, re, itertools, nltk, codecs
 from decimal import Decimal
 from profiles.models import Profile
--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-# Create your views here.
+from .models import Profile
+
+def full(request, id):
+    try:
+        profile = Profile.objects.get(pk=id)
+    except Profile.DoesNotExist:
+        raise Http404("Profile does not exist")
+    return render(request, 'profiles/full.html', {'profile': profile})
